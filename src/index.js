26c1a0c58156dc07324c396c2faa10d160ba8edd
Brett Connell <dev@bretta.io>
2023-09-17 08:54:19 -0400

    resarting project - got into modulation and packaging too quickly beofre nailing nfundamentals

--- src/index.js
+++ src/index.js
@@ -1,25 +1,10 @@
-// Import dotenv to load environment variables from .env
-require('dotenv').config();
-
-// Import the specific Firebase services you need
-import { initializeApp } from 'firebase/app';
-import 'firebase/auth'; // Import other Firebase services you need
-import _ from 'lodash';
-
-// Import Firebase configuration from the separate file
-import firebaseConfig from './firebase-config'; // Adjust the path as needed
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-// Your existing component function
 function component() {
-  const element = document.createElement('div');
+    const element = document.createElement('div');
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  return element;
+    return element;
 }
 
 document.body.appendChild(component());
