6378873f3837abd747aab3d6787995884ded3781
Brett Connell <dev@bretta.io>
2023-09-18 09:45:01 -0400

    local to git 2023-09-18  9:45:01.05

---
+++ first.bat
@@ -0,0 +1,12 @@
+@echo off
+
+rem Navigate to your Git repository directory
+cd /d C:\brettaio\webpack
+
+rem Git commands
+git add .
+git commit -m "local to git %date% %time%"
+git push -u origin main
+
+rem Exit the script
+exit /b 0
