8cdd64ac78af5d0a66420dcce79d24ad2a102c18
Brett Connell <dev@bretta.io>
2023-09-17 21:51:53 -0400

    added an updated webpack.config, to scss, babelled js setup with echo. > .babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,35 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
-            },
-        ],
-    },
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'), // Output directory for bundled files
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader', // Use Babel to transpile JavaScript
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'], // Load SCSS styles
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // Use your HTML template
+    }),
+    new Dotenv(), // Load environment variables from .env
+  ],
+  devServer: {
+    contentBase: './dist', // Serve from the 'dist' directory
+    port: 3000, // Port for local development
+    open: true, // Automatically open in the default browser
+  },
 };
