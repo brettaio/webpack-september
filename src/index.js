42c9ff0fe56a4b22e0dc526e358b0d8d2974f6b3
Brett Connell <dev@bretta.io>
2023-09-17 22:57:41 -0400

    analytics was working on local host, putting up to get internet connect

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
 import _ from 'lodash';
 import './style.scss';
 
@@ -19,91 +21,29 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
-
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
 
 // Your Firebase-related code here
-var firebase = require('firebase');
-var firebaseui = require('firebaseui');
-
-// Initialize the FirebaseUI Widget using Firebase.
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
-
-ui.start('#firebaseui-auth-container', {
-  signInOptions: [
-    firebase.auth.EmailAuthProvider.PROVIDER_ID
-  ],
-  // Other config options...
-});
-
-
-ui.start('#firebaseui-auth-container', {
-  signInOptions: [
-    {
-      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      requireDisplayName: false
-    }
-  ]
-});
-
-ui.start('#firebaseui-auth-container', {
-  signInOptions: [
-    {
-      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD
-    }
-  ],
-  // Other config options...
-});
-
-// Is there an email link sign-in?
-if (ui.isPendingRedirect()) {
-  ui.start('#firebaseui-auth-container', uiConfig);
-}
-// This can also be done via:
-if (firebase.auth().isSignInWithEmailLink(window.location.href)) {
-  ui.start('#firebaseui-auth-container', uiConfig);
-}
-
-ui.start('#firebaseui-auth-container', {
-  signInOptions: [
-    {
-      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD,
-      // Allow the user the ability to complete sign-in cross device,
-      // including the mobile apps specified in the ActionCodeSettings
-      // object below.
-      forceSameDevice: false,
-      // Used to define the optional firebase.auth.ActionCodeSettings if
-      // additional state needs to be passed along request and whether to open
-      // the link in a mobile app if it is installed.
-      emailLinkSignIn: function() {
-        return {
-          // Additional state showPromo=1234 can be retrieved from URL on
-          // sign-in completion in signInSuccess callback by checking
-          // window.location.href.
-          url: 'https://www.example.com/completeSignIn?showPromo=1234',
-          // Custom FDL domain.
-          dynamicLinkDomain: 'example.page.link',
-          // Always true for email link sign-in.
-          handleCodeInApp: true,
-          // Whether to handle link in iOS app if installed.
-          iOS: {
-            bundleId: 'com.example.ios'
-          },
-          // Whether to handle link in Android app if opened in an Android
-          // device.
-          android: {
-            packageName: 'com.example.android',
-            installApp: true,
-            minimumVersion: '12'
-          }
-        };
-      }
-    }
-  ]
-});
-
 
 //
 
